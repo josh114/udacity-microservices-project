6d174b7079790b1472230c7b82bf56c183257ed4
josh114 <freejosh02@gmail.com>
2022-11-15 16:43:25 +0100

    refactored monolith to microservice

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.chkej4sr8sbu.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::bucket-2053815-dev2
+export AWS_BUCKET=bucket-2053815-dev2
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
